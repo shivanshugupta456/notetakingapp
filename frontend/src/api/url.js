a26d642f96f9beda7d4bd9d13b580d509a283ebe
shivanshu gupta <rajkishoregupta716@gmail.com>
2026-04-05 18:23:28 +0530

    fhneif

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const BACKEND_URL = axios.create({
-    baseURL: "http://localhost:4001/api/v1/noteapp/"
+    baseURL: import.meta.env.VITE_BACKEND_URL
 })
 
 export default BACKEND_URL
